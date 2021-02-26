e0252b83c793885cba76c7e4c8cdee141d4d0f0e
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-02-26 01:26:59 -0300

    added meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,15 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-            rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+
+          <meta property="og:title" content="move.it" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://moveit-six.vercel.app/" />
+          <meta property="og:image" content="/favicon.png" />
+          <meta property="og:description" content="Um projeto para você manter o foco no seu trabalho em ciclos, 
+          separados por pequenos desafios." />
+          <meta name="theme-color" content="#FF0000" />
         </Head>
 
         <body>
