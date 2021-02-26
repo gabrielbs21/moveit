e2014a6c45314782ba98a657cd54abb738c8a3a1
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-02-26 01:44:33 -0300

    fix profile image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://www.flaticon.com/svg/vstatic/svg/3237/3237472.svg?token=exp=1614311964~hmac=473a8e8719d7d8bd5de835f8d24a2cb8" alt="Gabriel Santos" />
+      <img src="/user.png" alt="Gabriel Santos" />
       <div>
         <strong>Usuário do move.it</strong>
         <p>
